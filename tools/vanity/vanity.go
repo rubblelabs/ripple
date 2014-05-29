76d440a7c2a2a38aecdc82dda6de560833281184
Donovan Hide <donovanhide@gmail.com>
2014-05-29 22:15:42 +0100

    Simple vanity address generator example

---
+++ tools/vanity/vanity.go
@@ -0,0 +1,51 @@
+// Simple tool to find the seeds for ripple account ids which match a regular expression
+package main
+
+import (
+	"flag"
+	"github.com/donovanhide/ripple/crypto"
+	"log"
+	"os"
+	"os/signal"
+	"regexp"
+	"runtime"
+)
+
+var name = flag.String("name", "ripple", "desired name to appear in ripple account id")
+var insensitive = flag.Bool("insenstive", true, "ignore case sensitity")
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+}
+
+func search(target *regexp.Regexp) {
+	for {
+		key, err := crypto.GenerateRootDeterministicKey(nil)
+		checkErr(err)
+		account, err := key.GenerateAccountId(0)
+		checkErr(err)
+		if target.MatchString(account.ToJSON()) {
+			log.Println(key.Seed.ToJSON(), account.ToJSON())
+		}
+	}
+}
+
+func main() {
+	flag.Parse()
+	match := *name
+	if *insensitive {
+		match = "(?i)" + match
+	}
+	target, err := regexp.Compile(match)
+	checkErr(err)
+	kill := make(chan os.Signal, 1)
+	signal.Notify(kill, os.Interrupt, os.Kill)
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	log.Printf("Searching for \"%s\" with %d processors", *name, runtime.NumCPU())
+	for i := 0; i < runtime.NumCPU(); i++ {
+		go search(target)
+	}
+	<-kill
+}
