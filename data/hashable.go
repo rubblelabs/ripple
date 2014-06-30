9c7f3e9f59ee345f27f2beb0e26f9a8825dad588
Donovan Hide <donovanhide@gmail.com>
2014-06-30 19:49:20 +0100

    Remove Raw() from hash able

--- data/hashable.go
+++ data/hashable.go
@@ -2,10 +2,7 @@ package data
 
 type hashable struct {
 	hash Hash256
-	raw  []byte
 }
 
 func (h *hashable) Hash() Hash256       { return h.hash }
-func (h *hashable) Raw() []byte         { return h.raw }
 func (h *hashable) SetHash(hash []byte) { copy(h.hash[:], hash[:]) }
-func (h *hashable) SetRaw(raw []byte)   { h.raw = make([]byte, len(raw)); copy(h.raw, raw) }
