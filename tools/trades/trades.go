66e40b667e50933de473bd533203a0b6f6e8664b
Donovan Hide <donovanhide@gmail.com>
2017-07-04 19:02:50 +0100

    Add historic downloading to trades tool

--- tools/trades/trades.go
+++ tools/trades/trades.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/rubblelabs/ripple/data"
 	"github.com/rubblelabs/ripple/websockets"
@@ -23,20 +25,7 @@ var (
 	account = flag.String("account", "", "optional account to monitor")
 )
 
-func main() {
-	flag.Parse()
-	var (
-		filter *data.Account
-		err    error
-	)
-	if len(*account) > 0 {
-		filter, err = data.NewAccountFromAddress(*account)
-		checkErr(err, true)
-	}
-
-	r, err := websockets.NewRemote(*host)
-	checkErr(err, true)
-
+func stream(r *websockets.Remote, filter *data.Account) {
 	confirmation, err := r.Subscribe(true, true, false, false)
 	checkErr(err, true)
 	log.Printf("Subscribed at: %d ", confirmation.LedgerSequence)
@@ -58,5 +47,58 @@ func main() {
 				log.Println(trade)
 			}
 		}
+	}
+}
+
+func download(r *websockets.Remote, start, end uint32, filter *data.Account) {
+	for ledger := start; ledger <= end; ledger++ {
+		result, err := r.Ledger(ledger, true)
+		checkErr(err, true)
+		for _, tx := range result.Ledger.Transactions {
+			tx.LedgerSequence = result.Ledger.LedgerSequence
+			trades, err := data.NewTradeSlice(tx)
+			checkErr(err, true)
+			if filter != nil {
+				trades = trades.Filter(*filter)
+			}
+			for _, trade := range trades {
+				fmt.Println(trade)
+			}
+		}
+	}
+}
+
+func parseledger(s string) uint32 {
+	ledger, err := strconv.ParseUint(s, 10, 32)
+	if err != nil {
+		log.Fatalf("bad ledger: %s", s)
+	}
+	return uint32(ledger)
+}
+
+func main() {
+	flag.Parse()
+	var (
+		filter *data.Account
+		err    error
+	)
+	if len(*account) > 0 {
+		filter, err = data.NewAccountFromAddress(*account)
+		checkErr(err, true)
+	}
+
+	r, err := websockets.NewRemote(*host)
+	checkErr(err, true)
+	switch flag.NArg() {
+	case 0:
+		stream(r, filter)
+	case 1:
+		ledger := parseledger(flag.Arg(0))
+		download(r, ledger, ledger, filter)
+	case 2:
+		start, end := parseledger(flag.Arg(0)), parseledger(flag.Arg(1))
+		download(r, start, end, filter)
+	default:
+		log.Fatalf("bad number of arguments: %d", flag.NArg())
 	}
 }
