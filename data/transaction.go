81ddd0cc959271504e37bdbcf5a0883087741129
Robert Zhang <robert.zhangle@gmail.com>
2018-04-11 01:02:37 -0700

    Add 'Date RippleTime' to TxBase

--- data/transaction.go
+++ data/transaction.go
@@ -7,6 +7,7 @@ type TxBase struct {
 	Account            Account
 	Sequence           uint32
 	Fee                Value
+	Date               RippleTime
 	AccountTxnID       *Hash256        `json:",omitempty"`
 	SigningPubKey      *PublicKey      `json:",omitempty"`
 	TxnSignature       *VariableLength `json:",omitempty"`
