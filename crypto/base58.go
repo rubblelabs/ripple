29af50dcf30e1ce1b550134e068b69a3881dab3b
Donovan Hide <donovanhide@gmail.com>
2022-04-15 07:34:33 +0100

    Update base58.go

--- crypto/base58.go
+++ crypto/base58.go
@@ -7,10 +7,25 @@ import (
 	"strings"
 )
 
-// Purloined from https://github.com/conformal/btcutil/
+// Purloined from https://github.com/btcsuite/btcd/blob/master/btcutil/base58/base58.go
 
-var bigRadix = big.NewInt(58)
-var bigZero = big.NewInt(0)
+var (
+	bigRadix10 = big.NewInt(58 * 58 * 58 * 58 * 58 * 58 * 58 * 58 * 58 * 58) // 58^10
+	bigZero    = big.NewInt(0)
+	bigRadix   = [...]*big.Int{
+		big.NewInt(0),
+		big.NewInt(58),
+		big.NewInt(58 * 58),
+		big.NewInt(58 * 58 * 58),
+		big.NewInt(58 * 58 * 58 * 58),
+		big.NewInt(58 * 58 * 58 * 58 * 58),
+		big.NewInt(58 * 58 * 58 * 58 * 58 * 58),
+		big.NewInt(58 * 58 * 58 * 58 * 58 * 58 * 58),
+		big.NewInt(58 * 58 * 58 * 58 * 58 * 58 * 58 * 58),
+		big.NewInt(58 * 58 * 58 * 58 * 58 * 58 * 58 * 58 * 58),
+		bigRadix10,
+	}
+)
 
 // Base58Decode decodes a modified base58 string to a byte slice and checks checksum.
 func Base58Decode(b, alphabet string) ([]byte, error) {
@@ -18,19 +33,44 @@ func Base58Decode(b, alphabet string) ([]byte, error) {
 		return nil, fmt.Errorf("Base58 string too short: %s", b)
 	}
 	answer := big.NewInt(0)
-	j := big.NewInt(1)
+	scratch := new(big.Int)
 
-	for i := len(b) - 1; i >= 0; i-- {
-		tmp := strings.IndexAny(alphabet, string(b[i]))
-		if tmp == -1 {
-			return nil, fmt.Errorf("Bad Base58 string: %s", b)
+	// Calculating with big.Int is slow for each iteration.
+	//    x += b58[b[i]] * j
+	//    j *= 58
+	//
+	// Instead we can try to do as much calculations on int64.
+	// We can represent a 10 digit base58 number using an int64.
+	//
+	// Hence we'll try to convert 10, base58 digits at a time.
+	// The rough idea is to calculate `t`, such that:
+	//
+	//   t := b58[b[i+9]] * 58^9 ... + b58[b[i+1]] * 58^1 + b58[b[i]] * 58^0
+	//   x *= 58^10
+	//   x += t
+	//
+	// Of course, in addition, we'll need to handle boundary condition when `b` is not multiple of 58^10.
+	// In that case we'll use the bigRadix[n] lookup for the appropriate power.
+	for t := b; len(t) > 0; {
+		n := len(t)
+		if n > 10 {
+			n = 10
 		}
-		idx := big.NewInt(int64(tmp))
-		tmp1 := big.NewInt(0)
-		tmp1.Mul(j, idx)
 
-		answer.Add(answer, tmp1)
-		j.Mul(j, bigRadix)
+		total := uint64(0)
+		for _, v := range t[:n] {
+			tmp := strings.Index(alphabet, string(v))
+			if tmp == -1 {
+				return nil, fmt.Errorf("Bad Base58 string: %s", b)
+			}
+			total = total*58 + uint64(tmp)
+		}
+
+		answer.Mul(answer, bigRadix[n])
+		scratch.SetUint64(total)
+		answer.Add(answer, scratch)
+
+		t = t[n:]
 	}
 
 	tmpval := answer.Bytes()
@@ -42,7 +82,7 @@ func Base58Decode(b, alphabet string) ([]byte, error) {
 		}
 	}
 	flen := numZeros + len(tmpval)
-	val := make([]byte, flen, flen)
+	val := make([]byte, flen)
 	copy(val[numZeros:], tmpval)
 
 	// Check checksum
@@ -61,11 +101,35 @@ func Base58Encode(b []byte, alphabet string) string {
 	x := new(big.Int)
 	x.SetBytes(b)
 
-	answer := make([]byte, 0)
-	for x.Cmp(bigZero) > 0 {
-		mod := new(big.Int)
-		x.DivMod(x, bigRadix, mod)
-		answer = append(answer, alphabet[mod.Int64()])
+	// maximum length of output is log58(2^(8*len(b))) == len(b) * 8 / log(58)
+	maxlen := int(float64(len(b))*1.365658237309761) + 1
+	answer := make([]byte, 0, maxlen)
+	mod := new(big.Int)
+	for x.Sign() > 0 {
+		// Calculating with big.Int is slow for each iteration.
+		//    x, mod = x / 58, x % 58
+		//
+		// Instead we can try to do as much calculations on int64.
+		//    x, mod = x / 58^10, x % 58^10
+		//
+		// Which will give us mod, which is 10 digit base58 number.
+		// We'll loop that 10 times to convert to the answer.
+
+		x.DivMod(x, bigRadix10, mod)
+		if x.Sign() == 0 {
+			// When x = 0, we need to ensure we don't add any extra zeros.
+			m := mod.Int64()
+			for m > 0 {
+				answer = append(answer, alphabet[m%58])
+				m /= 58
+			}
+		} else {
+			m := mod.Int64()
+			for i := 0; i < 10; i++ {
+				answer = append(answer, alphabet[m%58])
+				m /= 58
+			}
+		}
 	}
 
 	// leading zero bytes
@@ -81,6 +145,5 @@ func Base58Encode(b []byte, alphabet string) string {
 	for i := 0; i < alen/2; i++ {
 		answer[i], answer[alen-1-i] = answer[alen-1-i], answer[i]
 	}
-
 	return string(answer)
 }
